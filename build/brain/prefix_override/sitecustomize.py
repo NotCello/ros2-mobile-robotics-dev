90ad7b4b49a27a46b7809c86929742a3b1671716
NotCello <marcello12ori@gmail.com>
2025-12-12 01:42:39 +0100

    fixed some values

--- build/brain/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/utente/Documenti/Magistrale/SecondoAnno/EXP_RO/EXP_Ws/src/brain/install/brain'
